be3ba6c7ba94ada559643447dcc5d7dc9d8fb583
Karen V <karenvo@usc.edu>
2019-12-28 04:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,6 @@
 // `JavaScript is so cool. It lets me add text to my page programmatically.`
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  text = document.getElementById("text")
+  text.innerHTML = "This is really cool!";
 });
-
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
-
-document.getElementById("text").innerHTML = "This is really cool!"
